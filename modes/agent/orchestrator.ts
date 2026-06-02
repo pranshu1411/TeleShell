8065fa91d81a3e194783dfbf0edb7b25d9ab0e8a
Pranshu Gupta <pranshu.141104@gmail.com>
2026-06-02 23:16:27 +0530

    main file for agent mode

---
+++ modes/agent/orchestrator.ts
@@ -0,0 +1,19 @@
+import { isCancel, text } from "@clack/prompts";
+import chalk from "chalk";
+import { defaultAgentConfig } from "./types";
+import { ActionTracker } from "./action-tracer";
+
+export async function runAgentMode() {
+    console.log(chalk.bold('\n Agent Mode \n'));
+
+    const goal = await text({
+        message: "What would you like to do?",
+        placeholder: "create a new todo..."
+    });
+    if (isCancel(goal) || !goal.trim) {
+        return;
+    }
+
+    const config = defaultAgentConfig();
+    const tracker = new ActionTracker();
+}
